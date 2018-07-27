11d802b417a4c04111e65dd986a3180e34e6128d
Jordi <mumismo@gmail.com>
2018-07-27 20:28:27 +0200

    En vez de un menu usamos un boton para salir. No hace falta implementar ActionListener. Borrar codigo comentado

--- src/Ventana2.java
+++ src/Ventana2.java
@@ -6,10 +6,10 @@ import java.util.HashSet;
 import java.sql.SQLException;
 
 @SuppressWarnings("serial")
-public class Ventana2 extends JFrame implements ActionListener {
+public class Ventana2 extends JFrame {
     JPanel panel;
     JButton nuevo;
-    JButton buscar;
+    JButton salir;
 
     //en ventana de busqueda
     JLabel nombre;
@@ -18,9 +18,7 @@ public class Ventana2 extends JFrame implements ActionListener {
     JTextField ape;
     JButton bus;
 
-    MenuBar menuBar= new MenuBar();
     Connection conn;
-
 
     public Ventana2(Conexion conn) {
         this.conn=conn.conexion();
@@ -40,139 +38,57 @@ public class Ventana2 extends JFrame implements ActionListener {
     }
 
     private JPanel initcomponent2() {
-        System.out.println("componentes2");
-
-        //componentes de la ventana2
-
-        nuevo = new JButton("Nuevo Paciente");
-        nuevo.setBounds(50, 100, 160, 25);
-        nuevo.setToolTipText("Rellena un nuevo paciente");
-        buscar = new JButton("Buscar Paciente");
-        buscar.setBounds(500, 100, 160, 25);
-        buscar.setToolTipText("Busca un paciente");
-        nombre = new JLabel("Nombre:");
-        nombre.setForeground(Color.white);
-        nombre.setBounds(500,140,50,25);
-        name = new JTextField(20);
-        name.setBounds(550,140,100,25);
-        apellido = new JLabel("Apellido:");
-        apellido.setBounds(500,175,50,25);
-        apellido.setForeground(Color.white);
-        ape= new JTextField(20);
-        ape.setBounds(550,175,100,25);
-
-        bus = new JButton(new ImageIcon("C:\\Users\\Raistlin\\IdeaProjects\\control\\Interfaz clinica\\Imagenes\\buscar.jpg"));
-        bus.setBounds(550,250,50,50);
-
-    //setNemonic
-        //para el menu ejemplo close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
-
-        //ImageIcon newIcon=new ImageIcon(interface);
-
-        nuevo.addActionListener(this);
-        buscar.addActionListener(this);
-        name.addActionListener(this);
-//        apellido.addActionListener(this);
-        ape.addActionListener(this);
-        bus.addActionListener(this);
-
-
         panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(new Color(93,35,182));
         this.add(panel);
 
+        nuevo = new JButton("Nuevo Paciente"); 
+        nuevo.setBounds(50, 100, 160, 25);
+        nuevo.setToolTipText("Rellena un nuevo paciente");
+        nuevo.addActionListener(e -> nuevo());
         panel.add(nuevo);
-        panel.add(buscar);
 
+        salir = new JButton("Salir");
+        salir.setBounds(500, 500, 160, 25);
+        salir.setToolTipText("Salir de la aplicacion");
+        salir.addActionListener(e -> FisioNat.exit(0));
+        panel.add(salir);
 
-        MenuBar menuBar = new MenuBar();
-        menuBar.menuBar.setBounds(0,0,getWidth(),20);
-        panel.add(menuBar.menuBar);
+        nombre = new JLabel("Nombre:");
+        nombre.setForeground(Color.white);
+        nombre.setBounds(500,140,50,25);
+        panel.add(nombre);
+        name = new JTextField(20);
+        name.setBounds(550,140,100,25);
+        panel.add(name);
+        apellido = new JLabel("Apellido:");
+        apellido.setBounds(500,175,50,25);
+        apellido.setForeground(Color.white);
+        panel.add(apellido);
+        ape= new JTextField(20);
+        ape.setBounds(550,175,100,25);
+        panel.add(ape);
+
+        bus = new JButton(new ImageIcon("C:\\Users\\Raistlin\\IdeaProjects\\control\\Interfaz clinica\\Imagenes\\buscar.jpg"));
+        bus.setBounds(550,250,50,50);
+        bus.addActionListener(e -> consulta());
+        panel.add(bus);
+
         return panel;
     }
 
-    void consulta() throws InterruptedException, SQLException {
-        new TablaDatos(conn, name.getText(), ape.getText());
+    void consulta() {
+        try {new TablaDatos(conn, name.getText(), ape.getText());}
+        catch (InterruptedException e1) {e1.printStackTrace();}
+        catch (SQLException e1) {e1.printStackTrace();}
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==buscar){
-            panel.add(nombre);
-            panel.add(name);
-            panel.add(apellido);
-            panel.add(ape);
-            panel.add(bus);
-            panel.repaint();
+    private void nuevo() {
+        try {
+            new NuevoPaciente(conn);
+        } catch (SQLException e1) {
+            e1.printStackTrace();
         }
-        if (e.getSource()==bus){
-
-            try {consulta();}
-
-            catch (InterruptedException e1) {e1.printStackTrace();}
-
-            catch (SQLException e1) {e1.printStackTrace();}
-        }
-        if (e.getSource()==nuevo){
-            try {
-                new NuevoPaciente(conn);
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        }
-
-
-        /*if (e.getSource() == tabla) {
-            System.out.println("pulsando tabla");
-            mipanel2.setVisible(false);
-            this.dispose();
-            System.out.println(conn);
-            try {
-                TablaDatos nuevavent = new TablaDatos(conn);
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            } catch (InterruptedException e1) {
-                e1.printStackTrace();
-            }
-        }*/
-
-        /*else if (e.getSource()==colec){
-            try {
-                coleccion();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-
-        }
-        else if (e.getSource()==ins){
-            try {
-                volcar();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-
-        }
-
-        else if (e.getSource() == consulta) {
-            consulta();
-        }
-        else if (e.getSource()==update){
-            try {
-                actua();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        }
-        else if(e.getSource()==Delete){
-            try {
-                actua();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        }
-        else if (e.getSource()==boorrarcole){
-            borrar();
-        }*/
     }
 }
